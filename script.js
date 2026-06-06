29a6b0244a568b72c3c1822d960b69cc609c3718
Ronan Jaeger <ronannyaks92@gmail.com>
2026-06-06 14:31:24 +0200

    Use real product images

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 const products = [
-  { id: 1, name: "Nivea 72H Moisture Body Lotion", category: "Bodycare", brand: "Nivea", price: 14, colors: ["#f7f9ff", "#2d62c9"] },
-  { id: 2, name: "Garnier Micellar Cleansing Water", category: "Skincare", brand: "Garnier", price: 12, colors: ["#f2f8f5", "#9bcfbd"] },
-  { id: 3, name: "Garnier Vitamin C Brightening Cream", category: "Skincare", brand: "Garnier", price: 16, colors: ["#ffd447", "#fff4a6"] },
-  { id: 4, name: "Simple Vitamin C Glow Facial Wash", category: "Skincare", brand: "Simple", price: 13, colors: ["#ffffff", "#ffb321"] },
-  { id: 5, name: "Pantene Argan Infused Hair Oil", category: "Haircare", brand: "Pantene", price: 15, colors: ["#ffe28a", "#d5a523"] },
-  { id: 6, name: "L'Oreal Elvive Dream Lengths No Haircut Cream", category: "Haircare", brand: "L'Oreal", price: 17, colors: ["#f78c73", "#e22870"] },
-  { id: 7, name: "Color Wow Dream Coat Spray", category: "Haircare", brand: "Color Wow", price: 32, colors: ["#ef3c4c", "#ffb0ac"] },
-  { id: 8, name: "CHI 44 Iron Guard Heat Protection Spray", category: "Haircare", brand: "CHI", price: 24, colors: ["#f23a32", "#ff8b7e"] },
-  { id: 9, name: "e.l.f. Soft Glam Satin Foundation", category: "Makeup", brand: "e.l.f.", price: 16, colors: ["#d7a16d", "#6b3a23"] },
-  { id: 10, name: "e.l.f. Power Grip Primer", category: "Makeup", brand: "e.l.f.", price: 14, colors: ["#58d7c6", "#005f68"] },
-  { id: 11, name: "NYX The Face Glue Primer", category: "Makeup", brand: "NYX", price: 18, colors: ["#f5f7ff", "#ff4bb3"] },
-  { id: 12, name: "Maybelline Lasting Fix Setting Spray", category: "Makeup", brand: "Maybelline", price: 15, colors: ["#cc1e8f", "#26223f"] },
-  { id: 13, name: "Revolution Banana Powder", category: "Makeup", brand: "Revolution", price: 12, colors: ["#f7d15f", "#fff6c6"] },
-  { id: 14, name: "Too Faced Cloud Crush Blush", category: "Makeup", brand: "Too Faced", price: 29, colors: ["#ff9bc3", "#fff2b0"] },
-  { id: 15, name: "Milk Makeup Hydro Grip Primer", category: "Makeup", brand: "Milk Makeup", price: 34, colors: ["#d8f7e9", "#49b59c"] },
-  { id: 16, name: "Urban Decay All Nighter Setting Spray", category: "Makeup", brand: "Urban Decay", price: 36, colors: ["#3c114c", "#b85dff"] },
-  { id: 17, name: "Medicube Kojic Acid Line Set", category: "Skincare", brand: "Medicube", price: 28, colors: ["#ffc729", "#fff1ad"] },
-  { id: 18, name: "Medicube Collagen Night Wrapping Mask", category: "Skincare", brand: "Medicube", price: 30, colors: ["#553018", "#d7b46f"] },
-  { id: 19, name: "Jumiso Niacinamide 20 Serum", category: "Skincare", brand: "Jumiso", price: 22, colors: ["#5b0f54", "#d456a4"] },
-  { id: 20, name: "The Ordinary Niacinamide Serum", category: "Skincare", brand: "The Ordinary", price: 14, colors: ["#f5f5f5", "#c9c9c9"] },
-  { id: 21, name: "Alpha Skincare Moisturizing Body Wash", category: "Bodycare", brand: "Alpha", price: 19, colors: ["#d4fff0", "#66d0c3"] },
-  { id: 22, name: "Naturium The Glow Getter Body Wash", category: "Bodycare", brand: "Naturium", price: 21, colors: ["#fff1db", "#f8c456"] },
-  { id: 23, name: "EOS Cashmere Body Lotion", category: "Bodycare", brand: "EOS", price: 15, colors: ["#ff9eca", "#ffd2e6"] },
-  { id: 24, name: "EOS Vanilla Cashmere Body Oil", category: "Bodycare", brand: "EOS", price: 18, colors: ["#ead2ff", "#bb70e8"] },
-  { id: 25, name: "BYOMA Brightening Toner", category: "Skincare", brand: "BYOMA", price: 18, colors: ["#b8d7ff", "#68a6e8"] },
-  { id: 26, name: "BYOMA Hydrating Milky Toner", category: "Skincare", brand: "BYOMA", price: 18, colors: ["#fff8cb", "#dad064"] },
-  { id: 27, name: "Tree Hut Vanilla Sugar Scrub", category: "Bodycare", brand: "Tree Hut", price: 20, colors: ["#ff4d9a", "#ffb0d3"] },
-  { id: 28, name: "Bath & Body Works Mini Mist", category: "Fragrance", brand: "Bath & Body Works", price: 16, colors: ["#ff5b9d", "#ffcfdd"] },
-  { id: 29, name: "Hourglass Ambient Soft Glow Foundation", category: "Makeup", brand: "Hourglass", price: 48, colors: ["#d8a879", "#56351e"] },
-  { id: 30, name: "Charlotte Tilbury Magic Cream", category: "Skincare", brand: "Charlotte Tilbury", price: 55, colors: ["#f6d8c8", "#c98a64"] }
+  { id: 1, name: "Nivea 72H Moisture Body Lotion", category: "Bodycare", brand: "Nivea", price: 14, image: "https://img.nivea.com/-/media/miscellaneous/media-center-items/7/0/d/caa10679ba5d4e819e907aa468bc276d-web_1010x1180_transparent_png.png", colors: ["#f7f9ff", "#2d62c9"] },
+  { id: 2, name: "Garnier Micellar Cleansing Water", category: "Skincare", brand: "Garnier", price: 12, image: "https://media.ulta.com/i/ulta/2298445", colors: ["#f2f8f5", "#9bcfbd"] },
+  { id: 3, name: "Garnier Vitamin C Micellar Water", category: "Skincare", brand: "Garnier", price: 16, image: "https://media.ulta.com/i/ulta/2570035", colors: ["#ffd447", "#fff4a6"] },
+  { id: 4, name: "Simple Vitamin C Glow Facial Wash", category: "Skincare", brand: "Simple", price: 13, image: "https://onemg.gumlet.io/l_watermark_346%2Cw_480%2Ch_480/a_ignore%2Cw_480%2Ch_480%2Cc_fit%2Cq_auto%2Cf_auto/7a1d03a05ac349ee8cc5c690937234cd.jpg", colors: ["#ffffff", "#ffb321"] },
+  { id: 5, name: "Pantene Argan Infused Hair Oil", category: "Haircare", brand: "Pantene", price: 15, image: "https://boots.scene7.com/is/image/Boots/10225249", colors: ["#ffe28a", "#d5a523"] },
+  { id: 6, name: "L'Oreal Elvive Dream Lengths No Haircut Cream", category: "Haircare", brand: "L'Oreal", price: 17, image: "https://media.ulta.com/i/ulta/2621585", colors: ["#f78c73", "#e22870"] },
+  { id: 7, name: "Color Wow Dream Coat Spray", category: "Haircare", brand: "Color Wow", price: 32, image: "https://media.ulta.com/i/ulta/2589101", colors: ["#ef3c4c", "#ffb0ac"] },
+  { id: 8, name: "CHI 44 Iron Guard Heat Protection Spray", category: "Haircare", brand: "CHI", price: 24, image: "https://media.ulta.com/i/ulta/2113781", colors: ["#f23a32", "#ff8b7e"] },
+  { id: 9, name: "e.l.f. Soft Glam Satin Foundation", category: "Makeup", brand: "e.l.f.", price: 16, image: "https://media.ulta.com/i/ulta/2624374", colors: ["#d7a16d", "#6b3a23"] },
+  { id: 10, name: "e.l.f. Power Grip Primer", category: "Makeup", brand: "e.l.f.", price: 14, image: "https://media.ulta.com/i/ulta/2591795", colors: ["#58d7c6", "#005f68"] },
+  { id: 11, name: "NYX The Face Glue Primer", category: "Makeup", brand: "NYX", price: 18, image: "https://media.ulta.com/i/ulta/2636961", colors: ["#f5f7ff", "#ff4bb3"] },
+  { id: 12, name: "Maybelline Lasting Fix Setting Spray", category: "Makeup", brand: "Maybelline", price: 15, image: "https://media.ulta.com/i/ulta/2545896", colors: ["#cc1e8f", "#26223f"] },
+  { id: 13, name: "Revolution Banana Powder", category: "Makeup", brand: "Revolution", price: 12, image: "https://media.ulta.com/i/ulta/2545207", colors: ["#f7d15f", "#fff6c6"] },
+  { id: 14, name: "Too Faced Cloud Crush Blush", category: "Makeup", brand: "Too Faced", price: 29, image: "https://media.ulta.com/i/ulta/2603640", colors: ["#ff9bc3", "#fff2b0"] },
+  { id: 15, name: "Milk Makeup Hydro Grip Primer", category: "Makeup", brand: "Milk Makeup", price: 34, image: "https://media.ulta.com/i/ulta/2634881", colors: ["#d8f7e9", "#49b59c"] },
+  { id: 16, name: "Urban Decay All Nighter Setting Spray", category: "Makeup", brand: "Urban Decay", price: 36, image: "https://media.ulta.com/i/ulta/2642048", colors: ["#3c114c", "#b85dff"] },
+  { id: 17, name: "Medicube Kojic Acid Line Set", category: "Skincare", brand: "Medicube", price: 28, image: "https://media.ulta.com/i/ulta/2652448", colors: ["#ffc729", "#fff1ad"] },
+  { id: 18, name: "Medicube Collagen Night Wrapping Mask", category: "Skincare", brand: "Medicube", price: 30, image: "https://media.ulta.com/i/ulta/2645372", colors: ["#553018", "#d7b46f"] },
+  { id: 19, name: "Jumiso Niacinamide 20 Serum", category: "Skincare", brand: "Jumiso", price: 22, image: "https://media.ulta.com/i/ulta/2648959", colors: ["#5b0f54", "#d456a4"] },
+  { id: 20, name: "The Ordinary Niacinamide Serum", category: "Skincare", brand: "The Ordinary", price: 14, image: "https://media.ulta.com/i/ulta/2551167", colors: ["#f5f5f5", "#c9c9c9"] },
+  { id: 21, name: "Alpha Skincare Moisturizing Body Wash", category: "Bodycare", brand: "Alpha", price: 19, image: "https://media.ulta.com/i/ulta/77011565", colors: ["#d4fff0", "#66d0c3"] },
+  { id: 22, name: "Naturium The Glow Getter Body Wash", category: "Bodycare", brand: "Naturium", price: 21, image: "https://media.ulta.com/i/ulta/2627589", colors: ["#fff1db", "#f8c456"] },
+  { id: 23, name: "EOS Cashmere Body Lotion", category: "Bodycare", brand: "EOS", price: 15, image: "https://media.ulta.com/i/ulta/2653111", colors: ["#ff9eca", "#ffd2e6"] },
+  { id: 24, name: "EOS Vanilla Cashmere Body Wash", category: "Bodycare", brand: "EOS", price: 18, image: "https://media.ulta.com/i/ulta/2653114", colors: ["#ead2ff", "#bb70e8"] },
+  { id: 25, name: "BYOMA Brightening Toner", category: "Skincare", brand: "BYOMA", price: 18, image: "https://media.ulta.com/i/ulta/2611684", colors: ["#b8d7ff", "#68a6e8"] },
+  { id: 26, name: "BYOMA Hydrating Milky Toner", category: "Skincare", brand: "BYOMA", price: 18, image: "https://media.ulta.com/i/ulta/2647488", colors: ["#fff8cb", "#dad064"] },
+  { id: 27, name: "Tree Hut Vanilla Sugar Scrub", category: "Bodycare", brand: "Tree Hut", price: 20, image: "https://media.ulta.com/i/ulta/2652064", colors: ["#ff4d9a", "#ffb0d3"] },
+  { id: 28, name: "Bath & Body Works Champagne Toast Mist", category: "Fragrance", brand: "Bath & Body Works", price: 16, image: "https://www.bathandbodyworks.com/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw0f84e52f/hires/028019198_alt_1.jpg", colors: ["#ff5b9d", "#ffcfdd"] },
+  { id: 29, name: "Hourglass Ambient Soft Glow Foundation", category: "Makeup", brand: "Hourglass", price: 48, image: "https://media.ulta.com/i/ulta/2599670", colors: ["#d8a879", "#56351e"] },
+  { id: 30, name: "Charlotte Tilbury Magic Cream", category: "Skincare", brand: "Charlotte Tilbury", price: 55, image: "https://media.ulta.com/i/ulta/2652684", colors: ["#f6d8c8", "#c98a64"] }
 ];
 
 const state = {
@@ -63,6 +63,7 @@ function renderProducts() {
   productGrid.innerHTML = items.map((product) => `
     <article class="product-card">
       <div class="product-art" style="--card-a:${product.colors[0]}; --card-b:${product.colors[1]}">
+        <img class="product-image" src="${product.image}" alt="${product.brand} ${product.name}" loading="eager" onerror="this.hidden=true; this.nextElementSibling.style.display='block';">
         <div class="product-shape" aria-hidden="true"></div>
       </div>
       <div class="product-info">
